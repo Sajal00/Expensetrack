847e33763a78a78e7433ac57d954f7b3bd43d050
Sajal00 <sajalbairagi9@gmail.com>
2024-07-19 23:41:10 +0530

    Initial Setup

---
+++ src/AppNavigator/AppNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from '../Screens/SplashScreen';
+import LoginScreen from '../Screens/LoginScreen';
+import HomeScreen from '../Screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="Login">
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
