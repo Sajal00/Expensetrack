1ca9d189ec903a10e9450e79fc3f8da9711dcab4
Sajal00 <sajalbairagi9@gmail.com>
2024-07-22 21:38:42 +0530

    firebase notification

--- App.tsx
+++ App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppNavigator from './src/AppNavigator/AppNavigator';
 import {Provider} from 'react-redux';
 import Store from './src/Redux/Store/Store';
+import messaging from '@react-native-firebase/messaging';
+import {Alert} from 'react-native';
+
+const _getDeviceToken = async () => {
+  let token = await messaging().getToken();
+  console.log('device token from firebase is ', token);
+};
 
 const App = () => {
+  //
+  useEffect(() => {
+    _getDeviceToken();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert(
+        'A new FCM message arrived foreground mode!',
+        JSON.stringify(remoteMessage),
+      );
+    });
+    return unsubscribe;
+  }, []);
   return (
     <Provider store={Store}>
       <AppNavigator />
